67e73a6aac52048fa691c8ab2ba006dcc9e44c68
Derick Yang <dericky@mathworks.com>
2017-07-13 11:43:05 -0400

    format data nicely

--- getWordBags.m
+++ getWordBags.m
@@ -2,23 +2,25 @@ function t = getWordBags(data)
     bodies = data.articleBody;
     headlines = data.Headline;
     bsize = size(bodies);
-    bodyBags = cell(bsize(1), 1);
+    bodyCounts = cell(bsize(1), 1);
     headWords = cell(bsize(1), 1);
     for i = 1:bsize(1)
-        bodyBags{i} = getWordBag(bodies{i});
+        bodyCounts{i} = getWordBag(bodies{i});
         headWords{i} = getImportantHeadline(headlines{i});
     end
-    t = table(bodyBags, headWords, data.Stance);
+    t = table(bodyCounts, headWords, data.Stance);
 end
 
-function wordBag = getWordBag(articleBody)
+function wordCounts = getWordBag(articleBody)
     doc = tokenizedDocument(articleBody);
-%     doc = normalizeWords(doc);
     wordBag = bagOfWords(doc);
-    wordBag = removeWords(wordBag, stopWords);
+    wordBag = removeWords(wordBag, [stopWords,".","?","!",",",";",":"]);
+    fullCounts = full(wordBag.Counts);
+    wordCounts = [fullCounts; wordBag.Vocabulary];
 end
 
 function importantHeadline = getImportantHeadline(origHead)
     doc = tokenizedDocument(origHead);
-    importantHeadline = removeWords(doc, stopWords);
+    vocab = removeWords(doc, [stopWords,".","?","!",",",";",":"]);
+    importantHeadline = vocab.Vocabulary;
 end
