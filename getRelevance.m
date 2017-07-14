cf893fecd3690b094fbe63e3d8cc39b06ef1604a
mdoxey06 <mdoxey06@gmail.com>
2017-07-14 09:16:51 -0400

    separated code

--- getRelevance.m
+++ getRelevance.m
@@ -4,23 +4,26 @@ function table = getRelevance(table)
     headlines = table.headWords;
     headssize = size(headlines);
     for i = 1:headssize(1)
-        cumSum = 0;
-        wordsInHeadline = 0;
-        array = string(headlines{i});
-        hsize = size(array);
-        hsize = hsize(2);
-        body = bodyMap{i,1};
-        for j = 1:hsize
-            hKey = char(array(j));
-            if (isKey(body,hKey))
-                freq = body(hKey);
-                cumSum = cumSum + freq;
-                wordsInHeadline = wordsInHeadline + 1;
-            end
-        end
-        cumSumScore = cumSum/bodyWords{1,1};
-        wordsScore = wordsInHeadline/hsize;
+        [cumSumScore , wordsScore] = getScores(headlines{i}, bodyMap{i,1}, bodyWords);
         table.relevance(i) = cumSumScore;
         table.headlineWordsInArticle(i) = wordsScore;
     end
+end
+
+function [cumSumScore, wordsScore]  = getScores(headline, body, bodyWords)
+    cumSum = 0;
+    wordsInHeadline = 0;
+    array = string(headline);
+    hsize = size(array);
+    hsize = hsize(2);
+    for j = 1:hsize
+        hKey = char(array(j));
+        if (isKey(body,hKey))
+            freq = body(hKey);
+            cumSum = cumSum + freq;
+            wordsInHeadline = wordsInHeadline + 1;
+        end
+    end
+    cumSumScore = cumSum/bodyWords{1,1};
+    wordsScore = wordsInHeadline/hsize;
 end
